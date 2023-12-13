fae53be5cac1fa3d07e1fe2d7aefb6ae58e7241f
mingminyu <yu_mingm623@163.com>
2023-12-14 00:35:14 +0800

    Release 1.0

--- tools/init_project.py
+++ tools/init_project.py
@@ -10,12 +10,14 @@ def create_project_dirs(spk_id: str = "") -> None:
         if not os.path.exists(fst_dir):
             os.mkdir(fst_dir)
 
+    # 创建 audio 二级子目录
     audio_down_dirs = ["upload", "split", "denoise", "segments", "vad", "tts"]
     for audio_down_dir in audio_down_dirs:
         audio_down_dir_ = os.path.join("audio", audio_down_dir, spk_id)
         if not os.path.exists(audio_down_dir_):
             os.makedirs(audio_down_dir_)
 
+    # 创建 data 二级子目录
     data_down_dirs = ["asr", "trans", "train", "cleaned"]
     for data_down_dir in data_down_dirs:
         data_down_dir_ = os.path.join("data", data_down_dir, spk_id)
