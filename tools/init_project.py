9df8e810856f5a76e0800dd61372ca5e63862742
mingminyu <yu_mingm623@163.com>
2023-12-14 01:23:34 +0800

    Release 1.0

--- tools/init_project.py
+++ tools/init_project.py
@@ -1,4 +1,7 @@
 import os
+import whisper
+import wget
+from loguru import logger
 
 
 def create_project_dirs(spk_id: str = "") -> None:
@@ -23,3 +26,59 @@ def create_project_dirs(spk_id: str = "") -> None:
         data_down_dir_ = os.path.join("data", data_down_dir, spk_id)
         if not os.path.exists(data_down_dir_):
             os.makedirs(data_down_dir_)
+
+    if not os.path.exists("pretrained_models/whisper"):
+        os.makedirs("pretrained_models/whisper")
+
+
+def download_pretrained_models(
+    whisper_size: str = "medium",
+):
+    """下载预训练模型
+    只适用于可以联网环境下
+    """
+    # 下载 chinese-roberta-wwm-ext-large
+    logger.info("下载模型: chinese-roberta-wwm-ext-large")
+
+    roberta_model_urls = [
+        "https://huggingface.co/hfl/chinese-roberta-wwm-ext-large/resolve/main/tf_model.h5",
+        "https://huggingface.co/hfl/chinese-roberta-wwm-ext-large/resolve/main/flax_model.msgpack",
+        "https://huggingface.co/hfl/chinese-roberta-wwm-ext-large/resolve/main/pytorch_model.bin",
+        "https://huggingface.co/hfl/chinese-roberta-wwm-ext-large/resolve/main/config.json"
+    ]
+    _ = [
+        wget.download(url, "pretrained_models/chinese-roberta-wwm-ext-large")
+        for url in roberta_model_urls
+    ]
+    logger.info("下载完成: chinese-roberta-wwm-ext-large")
+
+    # 下载 speech_dfsmn_ans_psm_48k_causal
+    from modelscope import snapshot_download
+
+    logger.info("下载模型: speech_dfsmn_ans_psm_48k_causal")
+    snapshot_download(
+        model_id="damo/speech_dfsmn_ans_psm_48k_causal",
+        cache_dir="./pretrained_models"
+    )
+    logger.info("下载完成: speech_dfsmn_ans_psm_48k_causal")
+
+    # 下载 speech_fsmn_vad_zh-cn-16k-common-pytorch
+    logger.info("下载模型: speech_fsmn_vad_zh-cn-16k-common-pytorch")
+    snapshot_download(
+        model_id="damo/speech_fsmn_vad_zh-cn-16k-common-pytorch",
+        cache_dir="./pretrained_models"
+    )
+    logger.info("下载完成: speech_fsmn_vad_zh-cn-16k-common-pytorch")
+
+    # 下载 Whisper 模型
+    logger.info(f"下载模型: whisper {whisper_size}")
+    _ = whisper.load_model(
+        whisper_size, download_root="pretrained_models/whisper"
+    )
+    logger.info(f"下载完成: whisper {whisper_size}")
+    logger.warning("下载完请重启下 notebook，释放掉机器资源")
+
+
+
+
+
